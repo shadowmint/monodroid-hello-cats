f40d3515c73d16bf2a2e18318f17ae7ed0425aee
doug <linderd@iinet.net.au>
2012-10-24 19:22:46 +0800

    Made cats work on nexus tablet

--- HelloWorld.Android/Views/Home/CatsView.cs
+++ HelloWorld.Android/Views/Home/CatsView.cs
@@ -35,18 +35,20 @@ namespace HelloWorld.Views.Home
 		private Bitmap _cat;
 		private Bitmap _bubble;
 		private Random _r;
-		private Canvas _c;
+		private float _width;
+		private float _height;
 		private Paint _p = null;
 
 		private void Init(Canvas c)
 		{
 			if (_p == null) {
 				_cat = BitmapFactory.DecodeResource(Resources, Resource.Drawable.Cat);
-				_bubble = BitmapFactory.DecodeResource(Resources, Resource.Drawable.Cat);
+				_bubble = BitmapFactory.DecodeResource(Resources, Resource.Drawable.Bubble);
 				_r = new Random();
-				_c = c;
 				_p = new Paint();
 			}
+			_width = c.Width;
+			_height = c.Height;
 		}
 
 		protected override void OnDraw(Canvas c)
@@ -57,9 +59,16 @@ namespace HelloWorld.Views.Home
 			_p.Color = Color.White;
 			c.DrawPaint(_p);
 
-			var cat = BitmapFactory.DecodeResource(Resources, Resource.Drawable.Cat);
-			c.DrawBitmap(cat, c.Width / 2 - cat.Width / 2, c.Height / 2 - cat.Height / 2, _p);
-
+			float scale = (float) _cat.Width / (float) _cat.Height;
+			int new_height = c.Height * 3 / 4;
+			int new_width = (int) (new_height * scale);
+			c.DrawBitmap(
+				_cat, 
+				new Rect(0, 0, _cat.Width, _cat.Height),
+				new Rect(0, c.Width / 4, new_width, new_height + c.Width / 4),
+				//new Rect(0, (int) (c.Height-new_height), (int) new_width, (int) c.Height),
+				_p
+			);
 
 			foreach (var purr in _purrs) {
 				purr.Render(c, _p);
@@ -70,7 +79,8 @@ namespace HelloWorld.Views.Home
 		{
 			if (_p != null) {
 				var size = Resources.GetDimensionPixelSize(Resource.Dimension.custom_text_size);
-				_purrs.Add(new CatPurr(_c.Width, _c.Height, size, _bubble, _r));
+				_purrs.Add(new CatPurr(_width, _height, size, _bubble, _r));
+				Invalidate(); // Request redraw
 			}
 			return base.OnTouchEvent (e);
 		}
@@ -88,7 +98,7 @@ namespace HelloWorld.Views.Home
 		public CatPurr(float width, float height, float ts, Bitmap bubble, Random r)
 		{
 			_x = (float) r.NextDouble() * width;
-			_y = (float) r.NextDouble() * height;
+			_y = (float) r.NextDouble() * height * 1 / 4;
 			_bubble = bubble;
 			_color = RandomColor(r);
 			_textSize = ts;
@@ -99,12 +109,13 @@ namespace HelloWorld.Views.Home
 			p.Color = _color;
 			p.TextSize = _textSize;
 			p.AntiAlias = true;
-			c.DrawText ("Purrrr", _x + 25, _y + 25, p);
+			c.DrawText ("Purr", _x + _bubble.Width / 2 - p.MeasureText("Purr") / 2, _y + _bubble.Height / 2 + _textSize / 4, p);
 		}
 
 	    private Color RandomColor(Random r) {
 			int blue = (r.Next() % 255);
-			Color rtn = Color.Argb(0, 0, 0, blue);
+			int red = (r.Next() % 255);
+			Color rtn = Color.Argb(255, red, 0, blue);
 			return rtn;
 		}
 	}
